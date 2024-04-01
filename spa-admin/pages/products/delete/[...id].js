ddbece368e70d677f6e06e7ae91632d3e184cc0b
andythegril <95900562+andythegril@users.noreply.github.com>
2024-04-01 18:26:59 +0700

    Update [...id].js

--- spa-admin/pages/products/delete/[...id].js
+++ spa-admin/pages/products/delete/[...id].js
@@ -25,7 +25,7 @@ export default function DeleteProductPage() {
     }
     return (
         <Layout>
-            <h1 className="text-center">Do you really want to delete "{productInfo?.title}"?</h1>
+            <h1 className="text-center">Do you really want to delete &quot;"{productInfo?.title}"&quot;?</h1>
             <div className="flex gap-2 justify-center">
                 <button className="btn-red" onClick={deleteProduct}>Yes</button>
                 <button className="btn-default" onClick={goBack}>No</button>
